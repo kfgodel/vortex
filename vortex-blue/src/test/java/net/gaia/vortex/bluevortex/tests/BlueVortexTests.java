e4621f5dccb9bd079ef542227a7e316a77a5245b
Dario L Garcia <dario.garcia@ikari02>
2012-05-09 21:02:51 -0300

    OPEN - issue VTX-98: Definir en tests como seria el uso de vortex en memoria http://localhost:6660/jira/browse/VTX-98
    Commit inicial

---
+++ vortex-blue/src/test/java/net/gaia/vortex/bluevortex/tests/BlueVortexTests.java
@@ -0,0 +1,45 @@
+/**
+ * 09/05/2012 20:56:05 Copyright (C) 2011 Darío L. García
+ * 
+ * <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
+ * alt="Creative Commons License" style="border-width:0"
+ * src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />
+ * <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text"
+ * property="dct:title" rel="dct:type">Software</span> by <span
+ * xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Darío García</span> is
+ * licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative
+ * Commons Attribution 3.0 Unported License</a>.
+ */
+package net.gaia.vortex.bluevortex.tests;
+
+import net.gaia.vortex.bluevortex.api.BlueVortex;
+import net.gaia.vortex.bluevortex.impl.BlueVortexImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Esta clase prueba varios usos simples de blue vortex
+ * 
+ * @author D. García
+ */
+public class BlueVortexTests {
+
+	private BlueVortex vortex;
+
+	@Before
+	public void crearVortex() {
+		vortex = BlueVortexImpl.create();
+	}
+
+	@Test
+	public void deberiaPermitirEnviarUnMensajeAUnReceptorSinFiltros() {
+
+	}
+
+	@Test
+	public void deberiaPermitirFiltrarMensajesPorJerarquia() {
+
+	}
+
+}
