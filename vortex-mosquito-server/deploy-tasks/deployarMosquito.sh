27b0484d3e53a048cc65b68b7780ea8b0e22f84c
root@mosquito <kfgodel@gmail.com>
2012-07-11 01:45:35 -0300

    Ajustes al deploy de mosquito

--- vortex-mosquito-server/deploy-tasks/deployarMosquito.sh
+++ vortex-mosquito-server/deploy-tasks/deployarMosquito.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cp -R -f -v deploy-tasks/mosquito /archivos/vortex/vortex-mosquito-server
-cp -f -v target/vortex-mosquito-server-jar-with-dependencies.jar /archivos/vortex/vortex-mosquito-server/lib
+cp -rf deploy-tasks/mosquito /archivos/vortex/vortex-mosquito-server
+cp -rfv deploy-tasks/mosquito/* /archivos/vortex/vortex-mosquito-server/
+cp -fv target/vortex-mosquito-server-jar-with-dependencies.jar /archivos/vortex/vortex-mosquito-server/lib
