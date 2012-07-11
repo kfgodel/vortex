13a23863e11e4fb3258aff77c68f41e515324c0c
root@mosquito <kfgodel@gmail.com>
2012-07-11 01:10:33 -0300

    Archivos preparados para mosquito

--- vortex-mosquito-server/deploy-tasks/deployarMosquito.sh
+++ vortex-mosquito-server/deploy-tasks/deployarMosquito.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp target/vortex-mosquito-server-jar-with-dependencies.jar /archivos/vortex/vortex-mosquito-server/
+cp -R -f -v deploy-tasks/mosquito /archivos/vortex/vortex-mosquito-server
+cp -f -v target/vortex-mosquito-server-jar-with-dependencies.jar /archivos/vortex/vortex-mosquito-server/lib
