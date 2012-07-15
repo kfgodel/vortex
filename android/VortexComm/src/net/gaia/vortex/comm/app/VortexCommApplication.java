756209ee3efced65da884c22847adb98b1eb4dad
Dario L Garcia <dario.garcia@ikari02>
2012-07-14 23:04:54 -0300

    Agregado de aplicación

---
+++ android/VortexComm/src/net/gaia/vortex/comm/app/VortexCommApplication.java
@@ -0,0 +1,75 @@
+/**
+ * 14/07/2012 21:59:47 Copyright (C) 2011 Darío L. García
+ * 
+ * <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
+ * alt="Creative Commons License" style="border-width:0"
+ * src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />
+ * <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text"
+ * property="dct:title" rel="dct:type">Software</span> by <span
+ * xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Darío García</span> is
+ * licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative
+ * Commons Attribution 3.0 Unported License</a>.
+ */
+package net.gaia.vortex.comm.app;
+
+import net.gaia.taskprocessor.api.TaskProcessor;
+import net.gaia.taskprocessor.executor.ExecutorBasedTaskProcesor;
+import net.gaia.vortex.core.api.Nodo;
+import net.gaia.vortex.core.impl.atomos.forward.MultiplexorParalelo;
+import ar.com.iron.android.extensions.applications.CustomApplication;
+
+/**
+ * Esta clase representa la aplicación como punto goblal compartido
+ * 
+ * @author D. García
+ */
+public class VortexCommApplication extends CustomApplication {
+
+	private Nodo nodoDeAplicacion;
+	private TaskProcessor procesadorDeAplicacion;
+
+	public static VortexCommApplication I;
+
+	/**
+	 * @see ar.com.iron.android.extensions.applications.CustomApplication#getMainThreadName()
+	 */
+	@Override
+	protected String getMainThreadName() {
+		return "VortexCommThread";
+	}
+
+	public Nodo getNodoDeAplicacion() {
+		return nodoDeAplicacion;
+	}
+
+	public void setNodoDeAplicacion(Nodo nodoDeAplicacion) {
+		this.nodoDeAplicacion = nodoDeAplicacion;
+	}
+
+	public TaskProcessor getProcesadorDeAplicacion() {
+		return procesadorDeAplicacion;
+	}
+
+	public void setProcesadorDeAplicacion(TaskProcessor procesadorDeAplicacion) {
+		this.procesadorDeAplicacion = procesadorDeAplicacion;
+	}
+
+	/**
+	 * @see ar.com.iron.android.extensions.applications.CustomApplication#initializeGlobalComponents()
+	 */
+	@Override
+	protected void initializeGlobalComponents() {
+		procesadorDeAplicacion = ExecutorBasedTaskProcesor.createOptimun();
+		nodoDeAplicacion = MultiplexorParalelo.create(procesadorDeAplicacion);
+		I = this;
+	}
+
+	/**
+	 * @see ar.com.iron.android.extensions.applications.CustomApplication#shutdownGlobalComponents()
+	 */
+	@Override
+	public void shutdownGlobalComponents() {
+		procesadorDeAplicacion.detener();
+	}
+
+}
