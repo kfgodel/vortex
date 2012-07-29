9788bda66bd947bd249f9ca23e907bd351c76c04
Dario L Garcia <dario.garcia@ikari02>
2012-07-29 17:11:23 -0300

    Espacio extra

--- vortex-http/src/main/java/net/gaia/vortex/http/messages/PaqueteHttpVortex.java
+++ vortex-http/src/main/java/net/gaia/vortex/http/messages/PaqueteHttpVortex.java
@@ -25,7 +25,6 @@ import java.util.Map;
 public class PaqueteHttpVortex {
 
 	private List<Map<String, Object>> contenidos;
-
 	private Long proximaEsperaMinima;
 	private Long proximaEsperaMaxima;
 
