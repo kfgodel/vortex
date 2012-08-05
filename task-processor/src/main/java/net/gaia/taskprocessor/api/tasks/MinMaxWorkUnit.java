e390379d81f63c6de1ab357810352c04a8c44b52
Dario L Garcia <dario.garcia@ikari02>
2012-08-05 15:42:41 -0300

    Tarea recurrente con maximo y minimo de espera

---
+++ task-processor/src/main/java/net/gaia/taskprocessor/api/tasks/MinMaxWorkUnit.java
@@ -0,0 +1,226 @@
+/**
+ * 05/08/2012 04:26:23 Copyright (C) 2011 Darío L. García
+ * 
+ * <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
+ * alt="Creative Commons License" style="border-width:0"
+ * src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />
+ * <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text"
+ * property="dct:title" rel="dct:type">Software</span> by <span
+ * xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Darío García</span> is
+ * licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative
+ * Commons Attribution 3.0 Unported License</a>.
+ */
+package net.gaia.taskprocessor.api.tasks;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import net.gaia.taskprocessor.api.TaskCriteria;
+import net.gaia.taskprocessor.api.TaskProcessor;
+import net.gaia.taskprocessor.api.WorkUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ar.com.dgarcia.coding.exceptions.UnhandledConditionException;
+import ar.com.dgarcia.lang.strings.ToString;
+import ar.com.dgarcia.lang.time.TimeMagnitude;
+
+/**
+ * Esta clase representa una tarea que sólo puede ejecutarse dentro de un margen de tiempo. Ni
+ * demasiado pronto, ni demasiado tarde. Por lo tanto tiene un parámetro de mínima y uno de máxima
+ * respecto de cuánto tiempo es necesario esperar para ejecutar de nuevo.<br>
+ * Si se invoca antes del límite mínimo esta tarea no tiene efecto más que la re-planificación para
+ * ejecución próxima, si no se dentro del tiempo máxima de espera, la tarea se invoca a si misma
+ * antes de que se exceda el límite máximo.
+ * 
+ * @author D. García
+ */
+public class MinMaxWorkUnit implements WorkUnit {
+	private static final Logger LOG = LoggerFactory.getLogger(MinMaxWorkUnit.class);
+
+	private static final long MAXIMO_PERMITIDO_PARA_ADQUIRIR_LOCK = TimeMagnitude.of(10 * 60, TimeUnit.SECONDS)
+			.getMillis();
+
+	private long esperaMinima;
+	public static final String esperaMinima_FIELD = "esperaMinima";
+
+	private long esperaMaxima;
+	public static final String esperaMaxima_FIELD = "esperaMaxima";
+
+	private long momentoDeUltimaEjecucion;
+	public static final String momentoDeUltimaEjecucion_FIELD = "momentoDeUltimaEjecucion";
+
+	private TaskProcessor procesor;
+	private ReentrantLock lockDeEjecucion;
+
+	private WorkUnit tareaEjecutable;
+	public static final String tareaEjecutable_FIELD = "tareaEjecutable";
+
+	public static MinMaxWorkUnit crearWrapperDe(final WorkUnit delegada, final TaskProcessor processor,
+			final long esperaMinima, final long esperaMaxima) {
+		final MinMaxWorkUnit minmax = new MinMaxWorkUnit();
+		minmax.initialize(processor, delegada, esperaMinima, esperaMaxima);
+		return minmax;
+	}
+
+	/**
+	 * Inicializa el estado de esta instancia
+	 * 
+	 * @param delegada
+	 */
+	protected void initialize(final TaskProcessor procesor, final WorkUnit delegada, final long esperaMinima,
+			final long esperaMaxima) {
+		this.lockDeEjecucion = new ReentrantLock();
+		this.esperaMaxima = esperaMaxima;
+		this.esperaMinima = esperaMinima;
+		this.momentoDeUltimaEjecucion = 0;
+		this.procesor = procesor;
+		this.tareaEjecutable = delegada;
+	}
+
+	/**
+	 * @see net.gaia.taskprocessor.api.WorkUnit#doWork()
+	 */
+	@Override
+	public WorkUnit doWork() throws InterruptedException {
+		LOG.debug("Iniciando ejecucion min-max[{}]", this);
+		final boolean acquired = lockDeEjecucion.tryLock(MAXIMO_PERMITIDO_PARA_ADQUIRIR_LOCK, TimeUnit.MILLISECONDS);
+		if (!acquired) {
+			throw new UnhandledConditionException("No fue posible adquirir el lock para ejecutar la tarea[" + this
+					+ "]. Hay un thread bloqueado?");
+		}
+		try {
+			return ejecutarSincronizado();
+		} finally {
+			lockDeEjecucion.unlock();
+		}
+	}
+
+	/**
+	 * Ejecuta esta tarea asegurando que es el único thread
+	 * 
+	 * @return El resultado de la ejecución
+	 */
+	private WorkUnit ejecutarSincronizado() {
+		final long transcurridoDesdeUltimaEjecucion = getCurrentMillis() - momentoDeUltimaEjecucion;
+		final long milisFaltantesParaEjecucion = esperaMinima - transcurridoDesdeUltimaEjecucion;
+		if (milisFaltantesParaEjecucion > 0) {
+			LOG.debug("Ejecución prematura. Todavía faltan {} milis para ejecucion de [{}]",
+					milisFaltantesParaEjecucion, this);
+			// Todavía es necesario esperar para ejecutar esta tarea
+			replanificarEjecucionParaDentroDe(milisFaltantesParaEjecucion);
+			return null;
+		}
+		LOG.debug("Ejecucion aceptada. Ya pasaron {} milis desde ultima ejecucion de [{}]",
+				transcurridoDesdeUltimaEjecucion, this);
+		WorkUnit resultado;
+		try {
+			resultado = ejecutarTarea();
+		} catch (final Exception e) {
+			LOG.error("Se produjo un error ejecutando la tarea repetitiva[" + this
+					+ "]. Abortando ejecuciones posteriores", e);
+			return null;
+		}
+		momentoDeUltimaEjecucion = getCurrentMillis();
+		replanificarEjecucionParaDentroDe(esperaMaxima);
+		return resultado;
+	}
+
+	/**
+	 * Cancela todas las planificaciones de esta tarea a futuro
+	 */
+	private void cancelarPlanificacionesAnteriores() {
+		procesor.removeTasksMatching(new TaskCriteria() {
+			@Override
+			public boolean matches(final WorkUnit workUnit) {
+				return workUnit == MinMaxWorkUnit.this;
+			}
+		});
+	}
+
+	/**
+	 * Ejecuta la acción específica de esta tarea devolviendo una unidad para ejecución inmediata
+	 * como tarea posterior.<br>
+	 * Esta tarea será ejecutada nuevamente al ser invocada a posteriori entre espera mínima y
+	 * espera máxima
+	 * 
+	 * @return La continuación de esta tarea para ser agregada en la cola de tareas inmediatas o
+	 *         null si no hay continuación
+	 * @throws InterruptedException
+	 *             Si este thread es interrumpido esperando
+	 */
+	protected WorkUnit ejecutarTarea() throws InterruptedException {
+		final WorkUnit resultado = tareaEjecutable.doWork();
+		return resultado;
+	}
+
+	/**
+	 * Re-planifica la ejecución de esta tarea esperando los milisegundos indicados
+	 * 
+	 * @param esperaEnMilis
+	 *            La cantidad de milisegundos que debe esperar el procesador para ejecutar esta
+	 *            tarea
+	 */
+	private void replanificarEjecucionParaDentroDe(final long esperaEnMilis) {
+		cancelarPlanificacionesAnteriores();
+		LOG.debug("Proxima ejecucion en {} milis para [{}]", esperaEnMilis, this);
+		procesor.processDelayed(TimeMagnitude.of(esperaEnMilis, TimeUnit.MILLISECONDS), this);
+	}
+
+	/**
+	 * Devuelve el momento considerado actual en milisegundos
+	 * 
+	 * @return El timestamp actual
+	 */
+	public long getCurrentMillis() {
+		return System.currentTimeMillis();
+	}
+
+	public long getEsperaMinima() {
+		return esperaMinima;
+	}
+
+	public void setEsperaMinima(final long esperaMinima) {
+		this.esperaMinima = esperaMinima;
+	}
+
+	public long getEsperaMaxima() {
+		return esperaMaxima;
+	}
+
+	public void setEsperaMaxima(final long esperaMaxima) {
+		this.esperaMaxima = esperaMaxima;
+	}
+
+	public TaskProcessor getProcesor() {
+		return procesor;
+	}
+
+	public void setProcesor(final TaskProcessor procesor) {
+		this.procesor = procesor;
+	}
+
+	public long getMomentoDeUltimaEjecucion() {
+		return momentoDeUltimaEjecucion;
+	}
+
+	public WorkUnit getTareaEjecutable() {
+		return tareaEjecutable;
+	}
+
+	public void setTareaEjecutable(final WorkUnit tareaEjecutable) {
+		this.tareaEjecutable = tareaEjecutable;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return ToString.de(this).con(esperaMinima_FIELD, esperaMinima).con(esperaMaxima_FIELD, esperaMaxima)
+				.con(momentoDeUltimaEjecucion_FIELD, momentoDeUltimaEjecucion)
+				.con(tareaEjecutable_FIELD, tareaEjecutable).toString();
+	}
+
+}
---
+++ task-processor/src/test/java/net/gaia/taskprocessor/tests/tasks/TestMinMaxWorkUnit.java
@@ -0,0 +1,203 @@
+/**
+ * 05/08/2012 14:41:04 Copyright (C) 2011 Darío L. García
+ * 
+ * <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
+ * alt="Creative Commons License" style="border-width:0"
+ * src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />
+ * <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text"
+ * property="dct:title" rel="dct:type">Software</span> by <span
+ * xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Darío García</span> is
+ * licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative
+ * Commons Attribution 3.0 Unported License</a>.
+ */
+package net.gaia.taskprocessor.tests.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+import net.gaia.taskprocessor.api.TaskProcessor;
+import net.gaia.taskprocessor.api.WorkUnit;
+import net.gaia.taskprocessor.api.tasks.MinMaxWorkUnit;
+import net.gaia.taskprocessor.executor.ExecutorBasedTaskProcesor;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.com.dgarcia.lang.conc.WaitBarrier;
+import ar.com.dgarcia.lang.time.TimeMagnitude;
+
+/**
+ * Esta clase prueba la ejecución de las tareas con limite máximo-mínimo
+ * 
+ * @author D. García
+ */
+public class TestMinMaxWorkUnit {
+
+	private TaskProcessor procesor;
+
+	public static class TestWorkUnit implements WorkUnit {
+
+		public List<Long> ejecuciones = new ArrayList<Long>();
+		public WaitBarrier ejecutada = WaitBarrier.create();
+
+		/**
+		 * @see net.gaia.taskprocessor.api.WorkUnit#doWork()
+		 */
+		@Override
+		public WorkUnit doWork() throws InterruptedException {
+			final long currentMillis = System.currentTimeMillis();
+			ejecuciones.add(currentMillis);
+			ejecutada.release();
+			return null;
+		}
+
+	}
+
+	@Before
+	public void crearProcesador() {
+		procesor = ExecutorBasedTaskProcesor.create();
+	}
+
+	@After
+	public void detenerProcesador() {
+		procesor.detener();
+	}
+
+	@Test
+	public void deberiaEjecutarseInmediatamenteLaPrimeraVez() {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		Assert.assertEquals(0, testWorkUnit.ejecuciones.size());
+
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 10000, 10000);
+		procesor.process(minmax);
+
+		// Verificamos que se ejecuta inmediatamante
+		testWorkUnit.ejecutada.waitForReleaseUpTo(TimeMagnitude.of(30, TimeUnit.MILLISECONDS));
+		Assert.assertEquals(1, testWorkUnit.ejecuciones.size());
+	}
+
+	@Test
+	public void aunqueSePlanifiqueEjecucionPosteriorDeberiaEjecutarseAntesManualmenteLaPrimeraVez()
+			throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		Assert.assertEquals(0, testWorkUnit.ejecuciones.size());
+
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 10000, 10000);
+		procesor.processDelayed(TimeMagnitude.of(3, TimeUnit.SECONDS), minmax);
+
+		// Verificamos que todavía no se ejecutó
+		Thread.sleep(30);
+		Assert.assertEquals(0, testWorkUnit.ejecuciones.size());
+
+		procesor.process(minmax);
+		// Verificamos que se ejecuta inmediatamante
+		testWorkUnit.ejecutada.waitForReleaseUpTo(TimeMagnitude.of(30, TimeUnit.MILLISECONDS));
+		Assert.assertEquals(1, testWorkUnit.ejecuciones.size());
+	}
+
+	@Test
+	public void deberiarEjecutarseAutomaticamenteSiPasaElMaximoDeEspera() throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 0, 1000);
+		procesor.process(minmax);
+
+		Thread.sleep(1500);
+
+		// Verificamos que además de la primera se ejecutó una segunda vez al segundo
+		Assert.assertEquals(2, testWorkUnit.ejecuciones.size());
+	}
+
+	@Test
+	public void noDeberiaEjecutarseSiNoPasoElMinimoDeEspera() throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 3000, 3000);
+		// Ejecuta ahora
+		procesor.process(minmax);
+
+		// Intentamos una segunda ejecución manual que no tendrá efecto hasta los 3 segundos
+		procesor.process(minmax);
+		Thread.sleep(1500);
+
+		// Verificamos que la segunda ejecución no tuvo efecto
+		Assert.assertEquals(1, testWorkUnit.ejecuciones.size());
+	}
+
+	@Test
+	public void lasInvocacionesMientrasSeEjecutaNoTienenEfectoPorQueSonCanceladasAlReplanificar()
+			throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 1000, 3000);
+		// Ejecuta ahora
+		procesor.process(minmax);
+
+		// Intentamos una segunda ejecución manual que no tendrá efecto hasta el segundo
+		procesor.process(minmax);
+		Thread.sleep(1500);
+		// Verificamos que la segunda ejecución no tuvo efecto
+		Assert.assertEquals(1, testWorkUnit.ejecuciones.size());
+
+	}
+
+	@Test
+	public void alInvocarManualDespuesDeLaEjecucionYAntesDelMinimoSeReplanificaUnaEjecucionAutomaticamenteConElMinimoDeEspera()
+			throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 1000, 3000);
+		// Ejecuta ahora
+		procesor.process(minmax);
+
+		testWorkUnit.ejecutada.waitForReleaseUpTo(TimeMagnitude.of(500, TimeUnit.MILLISECONDS));
+
+		// Intentamos una segunda ejecución manual que no se ejecutará pero adelantará la ejecucion
+		procesor.process(minmax);
+		Thread.sleep(500);
+
+		// Verificamos que aun no se ejecutó la segunda
+		Assert.assertEquals(1, testWorkUnit.ejecuciones.size());
+
+		Thread.sleep(600);
+		// Verificamos que la segunda ejecucion se adelantó
+		Assert.assertEquals(2, testWorkUnit.ejecuciones.size());
+	}
+
+	@Test
+	public void deberiaEjecutarseSiYaPasoElMinimoYSeSolicitaSuEjecucionAntesDelMaximo() throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 1000, 3000);
+		// Ejecuta ahora
+		procesor.process(minmax);
+
+		// Esperamos que pase el minimo
+		Thread.sleep(1500);
+		// Intentamos una segunda ejecución manual que no tendrá efecto hasta el segundo
+		procesor.process(minmax);
+
+		// Verificamos que la segunda ejecución tuvo efecto
+		Thread.sleep(30);
+		Assert.assertEquals(2, testWorkUnit.ejecuciones.size());
+
+	}
+
+	@Test
+	public void alEjecutarseManualmenteDeberiaEsperarElMaximoParaEjecutarseNuevamente() throws InterruptedException {
+		final TestWorkUnit testWorkUnit = new TestWorkUnit();
+		final MinMaxWorkUnit minmax = MinMaxWorkUnit.crearWrapperDe(testWorkUnit, procesor, 1000, 3000);
+		// Ejecuta ahora
+		procesor.process(minmax);
+
+		// Esperamos que pase el mínimo
+		Thread.sleep(1500);
+		procesor.process(minmax);
+
+		// Esperamos que un poco menos del máximo
+		Thread.sleep(2500);
+
+		// Verificamos que aun no se ejecuto automaticamente porque se reseteo la espera
+		Assert.assertEquals(2, testWorkUnit.ejecuciones.size());
+
+	}
+
+}
