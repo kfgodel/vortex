a284b4e332d1603bf0f0651208e5881eb9b97170
Dario L Garcia <dario.garcia@ikari02>
2012-11-26 21:37:24 -0300

    Ajustes al deploy en ikarisrv02

--- vortex-mosquito-server/deploy-tasks/deployarIkariSrv.sh
+++ vortex-mosquito-server/deploy-tasks/deployarIkariSrv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp -rf deploy-tasks/mosquito /instalados/propios/vortex/vortex-mosquito-server
-cp -rfv deploy-tasks/mosquito/* /instalados/propios/vortex/vortex-mosquito-server/
+cp -rf deploy-tasks/ikariSrv02 /instalados/propios/vortex/vortex-mosquito-server
+cp -rfv deploy-tasks/ikariSrv02/* /instalados/propios/vortex/vortex-mosquito-server/
 cp -fv target/vortex-mosquito-server-jar-with-dependencies.jar /instalados/propios/vortex/vortex-mosquito-server/lib
