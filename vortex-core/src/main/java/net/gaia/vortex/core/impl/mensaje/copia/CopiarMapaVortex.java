57266c85f94560b8e003658b463ab0af79484ea9
Dario L Garcia <dario.garcia@ikari02>
2013-01-19 13:19:07 -0300

    http://192.168.1.135:6660/jira/browse/VTX-293
    Copia de mensajes

---
+++ vortex-core/src/main/java/net/gaia/vortex/core/impl/mensaje/copia/CopiarMapaVortex.java
@@ -0,0 +1,80 @@
+/**
+ * 19/01/2013 12:09:47 Copyright (C) 2011 Darío L. García
+ * 
+ * <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
+ * alt="Creative Commons License" style="border-width:0"
+ * src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />
+ * <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text"
+ * property="dct:title" rel="dct:type">Software</span> by <span
+ * xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Darío García</span> is
+ * licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative
+ * Commons Attribution 3.0 Unported License</a>.
+ */
+package net.gaia.vortex.core.impl.mensaje.copia;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import ar.com.dgarcia.colecciones.maps.impl.CaseInsensitiveHashMap;
+import ar.com.dgarcia.lang.strings.ToString;
+
+/**
+ * Esta clase representa la tarea de copia de un mapa con sus valores primitivos y submapas
+ * 
+ * @author D. García
+ */
+public class CopiarMapaVortex {
+
+	private Map<String, Object> mapaOriginal;
+	public static final String mapaOriginal_FIELD = "mapaOriginal";
+
+	private Map<String, Object> mapaCopia;
+	public static final String mapaCopia_FIELD = "mapaCopia";
+
+	public static CopiarMapaVortex create(final Map<String, Object> mapaOriginal, final Map<String, Object> mapaCopia) {
+		final CopiarMapaVortex copiar = new CopiarMapaVortex();
+		copiar.mapaCopia = mapaCopia;
+		copiar.mapaOriginal = mapaOriginal;
+		return copiar;
+	}
+
+	/**
+	 * Copia todos los valores del mapa original a la copia, generando tareas de copia pendiente por
+	 * cada mapa encontrado como valor
+	 * 
+	 * @return La lista de las copias pendientes por sub mapas
+	 */
+	public List<CopiarMapaVortex> copiar() {
+		final List<CopiarMapaVortex> copiasPendientes = new ArrayList<CopiarMapaVortex>();
+
+		final Set<Entry<String, Object>> entriesOriginal = mapaOriginal.entrySet();
+		for (final Entry<String, Object> entryOriginal : entriesOriginal) {
+			final String key = entryOriginal.getKey();
+			Object value = entryOriginal.getValue();
+			if (value instanceof Map) {
+				// Si es un mapa generamos otra copia para que sea modificable
+				@SuppressWarnings("unchecked")
+				final Map<String, Object> subMapaOriginal = (Map<String, Object>) value;
+				final CaseInsensitiveHashMap<Object> subMapaCopia = new CaseInsensitiveHashMap<Object>();
+				// La operación de copia queda para despues
+				copiasPendientes.add(CopiarMapaVortex.create(subMapaOriginal, subMapaCopia));
+				// Reemplazamos el valor para que se use el mapa que creamos
+				value = subMapaCopia;
+			}
+			mapaCopia.put(key, value);
+		}
+		return copiasPendientes;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return ToString.de(this).con(mapaOriginal_FIELD, mapaOriginal).con(mapaCopia_FIELD, mapaCopia).toString();
+	}
+
+}
---
+++ vortex-core/src/test/java/net/gaia/vortex/core/tests/TestClonador.java
@@ -0,0 +1,100 @@
+/**
+ * 19/01/2013 11:32:56 Copyright (C) 2011 Darío L. García
+ * 
+ * <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
+ * alt="Creative Commons License" style="border-width:0"
+ * src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />
+ * <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text"
+ * property="dct:title" rel="dct:type">Software</span> by <span
+ * xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Darío García</span> is
+ * licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative
+ * Commons Attribution 3.0 Unported License</a>.
+ */
+package net.gaia.vortex.core.tests;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import net.gaia.vortex.core.api.ids.componentes.IdDeComponenteVortex;
+import net.gaia.vortex.core.api.ids.mensajes.IdDeMensaje;
+import net.gaia.vortex.core.api.mensaje.ContenidoVortex;
+import net.gaia.vortex.core.api.mensaje.MensajeVortex;
+import net.gaia.vortex.core.impl.ids.componentes.GeneradorDeIdsGlobalesParaComponentes;
+import net.gaia.vortex.core.impl.ids.mensajes.GeneradorSecuencialDeIdDeMensaje;
+import net.gaia.vortex.core.impl.mensaje.ContenidoMapa;
+import net.gaia.vortex.core.impl.mensaje.MensajeConContenido;
+import net.gaia.vortex.core.impl.mensaje.copia.ClonadorDeMensajes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Esta clase prueba el clonador de mensajes
+ * 
+ * @author D. García
+ */
+public class TestClonador {
+
+	/**
+	 * Verifico que el contenido del mensaje sea igual pero no el mismo
+	 */
+	@Test
+	public void laCopiaDelMensajeDeberiaTenerUnaCopiaIgualDelContenido() {
+
+		final ContenidoVortex contenidoOriginal = ContenidoMapa.create();
+		contenidoOriginal.put("texto", "Hola");
+		contenidoOriginal.put("numero", 20L);
+		final HashMap<String, Object> mapa = new HashMap<String, Object>();
+		contenidoOriginal.put("mapa", mapa);
+		mapa.put("textoAnidado", "textoAnidado");
+		mapa.put("numero", 40L);
+
+		final MensajeConContenido mensajeOriginal = MensajeConContenido.create(contenidoOriginal);
+
+		// Le asignamos un ID
+		final IdDeComponenteVortex idDeComponente = GeneradorDeIdsGlobalesParaComponentes.getInstancia().generarId();
+		final IdDeMensaje idDeMensaje = GeneradorSecuencialDeIdDeMensaje.create(idDeComponente).generarId();
+		mensajeOriginal.asignarId(idDeMensaje);
+
+		final ClonadorDeMensajes clonador = ClonadorDeMensajes.create(mensajeOriginal);
+
+		final MensajeVortex mensajeCopia = clonador.clonar();
+		Assert.assertNotSame("Debería ser otra instancia de mensaje", mensajeOriginal, mensajeCopia);
+
+		final ContenidoVortex contenidoCopia = mensajeCopia.getContenido();
+		Assert.assertNotSame("Debería ser otra instancia de contenido", contenidoOriginal, contenidoCopia);
+
+		Assert.assertEquals("Deberian tener igual cantidad de claves", contenidoOriginal.size(), contenidoCopia.size());
+		final Set<Entry<String, Object>> entriesOriginal = contenidoOriginal.entrySet();
+		for (final Entry<String, Object> entryOriginal : entriesOriginal) {
+			final String claveOriginal = entryOriginal.getKey();
+			Assert.assertTrue("Debería existir la clave en la copia", contenidoCopia.containsKey(claveOriginal));
+
+			final Object valorOriginal = entryOriginal.getValue();
+			final Object valorCopia = contenidoCopia.get(claveOriginal);
+			Assert.assertEquals("Deberían ser valores equivalentes", valorOriginal, valorCopia);
+
+			if (valorOriginal instanceof Map) {
+				Assert.assertNotSame("Si el valor es un mapa debería ser una copia", valorOriginal, valorCopia);
+			}
+		}
+	}
+
+	@Test
+	public void laCopiaDeberiaMantenerElMismoID() {
+		final MensajeConContenido mensajeOriginal = MensajeConContenido.create(ContenidoMapa.create());
+		// Le asignamos un ID
+		final IdDeComponenteVortex idDeComponente = GeneradorDeIdsGlobalesParaComponentes.getInstancia().generarId();
+		final IdDeMensaje idDeMensaje = GeneradorSecuencialDeIdDeMensaje.create(idDeComponente).generarId();
+		mensajeOriginal.asignarId(idDeMensaje);
+
+		final ClonadorDeMensajes clonador = ClonadorDeMensajes.create(mensajeOriginal);
+
+		final MensajeVortex mensajeCopia = clonador.clonar();
+		final IdDeMensaje idDeLaCopia = mensajeCopia.getIdDeMensaje();
+		Assert.assertEquals("Debería tener el mismo valor de ID", idDeMensaje, idDeLaCopia);
+	}
+
+}
